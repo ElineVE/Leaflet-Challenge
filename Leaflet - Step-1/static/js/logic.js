54b93a339ef36f944c2376dfd8a9816cac2d3c57
Eline Van Eldere <elinevaneldere@gmail.com>
2020-09-23 16:09:41 -0400

    pushing adapted code, fixed legend up, fixed comments and colors

--- Leaflet - Step-1/static/js/logic.js
+++ Leaflet - Step-1/static/js/logic.js
@@ -10,7 +10,16 @@ function createMap(earthquakes) {
         id: "mapbox.streets",
         accessToken: api_key
     });
+    var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/satellite-v9",
+        accessToken: api_key
+    });
 
+    // Display layer on two different ways to view the maps
 
     var myMap = L.map("map", {
         center: [
@@ -19,19 +28,38 @@ function createMap(earthquakes) {
         zoom: 2,
         layers: [streetmap, earthquakes]
     });
-
-    // var baseMaps = {
-    //     "Light": lightmap,
-    //     "Dark": darkmap
-    // };
-
-    // var overlayMaps = {
-    //     Earthquakes: earthquakes
-    // };
+    var baseMaps = {
+        "Street Map": streetmap,
+        "Satellite": satellitemap
+    };
+    var overlayMaps = {
+        Earthquakes: earthquakes
+    };
 
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
+
+        // Add a colored legend to the map
+
     }).addTo(myMap);
+    var legend = L.control({ position: "bottomright" });
+
+    legend.onAdd = function() {
+
+        var div = L.DomUtil.create("div", "info legend");
+        grades = [0, 1, 2, 3, 4, 5];
+        colors = ["#98ee00", "green", "teal", "yellow", "orange", "red"];
+
+        // Looping through intervals, generating labels with colored square for each interval
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                "<i style='background: " + colors[i] + "'></i> " +
+                grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+        }
+        return div;
+    };
+
+    legend.addTo(myMap);
 
 }
 
@@ -45,29 +73,10 @@ var earthquakesurl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/
 d3.json(earthquakesurl, function(data) {
     createFeatures(data.features);
 
-    var legend = L.control({ position: "bottomright" });
+    ;
+})
 
-    legend.onAdd = function() {
-
-        var div = L.DomUtil.create("div", "info legend");
-        grades = [1, 2, 3, 4, 5];
-        colors = ["#90ee90", "green", "yellow", "orange", "red", "#5c0003"];
-
-        // Looping through intervals, generating labels with colored square for each interval
-        for (var i = 0; i < grades.length; i++) {
-            div.innerHTML +=
-                "<i style='background: " + colors[i] + "'></i> " +
-                grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
-        }
-        return div;
-    };
-
-    legend.addTo(myMap);
-
-});
-
-
-// Create a first layer for map: markers
+// Create a layer for the map
 
 function createFeatures(earthquakeData) {
 
@@ -75,6 +84,8 @@ function createFeatures(earthquakeData) {
         layer.bindPopup("<h3>" + feature.properties.place +
             "</h3><hr><p>" + new Date(feature.properties.time) +
             "</p><hr><p>" + "Magnitude: " + feature.properties.mag + "</p>");
+
+
     }
 
     var earthquakes = L.geoJSON(earthquakeData, {
@@ -82,23 +93,22 @@ function createFeatures(earthquakeData) {
         pointToLayer: function(feature, latlng) {
 
             var color = "";
-            if (Math.floor(feature.properties.mag) < 1) {
-                color = "#90ee90";
-            } else if (Math.floor(feature.properties.mag) < 2) {
-                color = "green";
-            } else if (Math.floor(feature.properties.mag) < 3) {
-                color = "yellow";
-            } else if (Math.floor(feature.properties.mag) < 4) {
-                color = "orange";
-            } else if (Math.floor(feature.properties.mag) < 5) {
+            if (Math.floor(feature.properties.mag) > 5) {
                 color = "red";
-            } else if (Math.floor(feature.properties.mag) < 6) {
-                color = "#5c0003";
-            }
+            } else if (Math.floor(feature.properties.mag) > 4) {
+                color = "orange";
+            } else if (Math.floor(feature.properties.mag) > 3) {
+                color = "yellow";
+            } else if (Math.floor(feature.properties.mag) > 2) {
+                color = "green";
+            } else if (Math.floor(feature.properties.mag) > 1) {
+                color = "teal";
+            } else { color = "#98ee00"; }
+
             var geojsonMarkerOptions = {
-                radius: feature.properties.mag * 7,
+                radius: feature.properties.mag * 4,
                 fillColor: color,
-                color: "black",
+                color: "#000000",
                 weight: 1,
                 opacity: 1,
                 fillOpacity: 0.8
